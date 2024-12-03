4eb226f03ee4e0f6a18448d6f1493da8eb040c77
Delioraz <134231875+Delioraz@users.noreply.github.com>
2024-12-03 19:37:26 +0000

    New project to test Git

--- src/git_2024343/Git_2024343.java
+++ src/git_2024343/Git_2024343.java
@@ -14,7 +14,8 @@ public class Git_2024343 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Matheus Pontes - 2024343");
     }
     
 }
